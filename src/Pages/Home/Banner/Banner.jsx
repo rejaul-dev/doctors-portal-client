1279a55337aad6f23be8c8ce85dfcd39cfaf2a49
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-14 16:08:34 +0800

    added navbar

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://placeimg.com/260/400/arch"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Routes/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../../Pages/Home/Home/Home";
+import Main from "../../layout/Main";
+import Login from "../../Pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
